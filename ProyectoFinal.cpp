cd4fec2c8cf61242144cb447f2589b0302282c3c
Emmanuelcorts <70960243+Emmanuelcorts@users.noreply.github.com>
2026-05-07 12:16:28 -0600

    Primer avance

--- ProyectoFinal.cpp
+++ ProyectoFinal.cpp
@@ -2,8 +2,25 @@
 Proyecto final 
 Crear juego de cartas(baraja inglesa)*/ 
 #include<iostream>
+#include<time.h>
  using namespace std;
+//Asignacion de cartas 
+srand(time(NULL));
+int jugadoresCant=0;
+
+cout"Cuantos jugadores desea registrar "endl; cin>>jugadoresCant;
+
+int asignacionCartas(int [2][5]);
 
  main(){
     
  }
+
+int asignacionCartas(int jugadoresCartas[][]){
+    for(int i =0;i<2;i++){
+       jugadoresCartas[i][j] =  rand() % 13+1;
+        for(int j=0;j<5;j++){
+            jugadoresCartas[i][j] = rand() % 4+1;
+        }
+    }
+}
